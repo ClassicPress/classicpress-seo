084c45d437b78086407179b792389de885302c76
Tim Hughes <timbocode@gmail.com>
2020-01-30 18:19:54 +0000

    Add missing properties to Services rich snippet

--- includes/modules/rich-snippet/snippets/class-service.php
+++ includes/modules/rich-snippet/snippets/class-service.php
@@ -29,17 +29,30 @@ class Service implements Snippet {
 	 */
 	public function process( $data, $jsonld ) {
 		$price  = Helper::get_post_meta( 'snippet_service_price' );
+		$phone_numbers = Helper::get_settings( 'titles.cpseo_phone_numbers' );
+		foreach ($phone_numbers as $phone) {
+			if ( isset ($phone['number']) && $phone['number'] != '' ) {
+				$telephone = $phone['number'];
+				break;
+			}
+		}
+		
 		$entity = [
 			'@context'        => 'https://schema.org',
 			'@type'           => 'Service',
 			'name'            => $jsonld->parts['title'],
 			'description'     => $jsonld->parts['desc'],
 			'serviceType'     => Helper::get_post_meta( 'snippet_service_type' ),
-			'offers'          => [
-				'@type'         => 'Offer',
-				'price'         => $price ? $price : '0',
-				'priceCurrency' => Helper::get_post_meta( 'snippet_service_price_currency' ),
-			],
+			'provider' 		  => [
+				'@type'		  => 'LocalBusiness',
+				'name'		  => $jsonld->get_website_name(),
+				'image'		  => Helper::get_settings( 'titles.cpseo_knowledgegraph_logo' ),
+				'address'	  => Helper::get_settings( 'titles.cpseo_local_address' ),
+				'telephone'   => $telephone,
+				'priceRange'  => Helper::get_settings( 'titles.cpseo_price_range' ),
+			 ],
+			'areaServed'	  => Helper::get_post_meta( 'snippet_service_area' ),
+			'url'			  => $jsonld->parts['url'],
 		];
 
 		return $entity;
