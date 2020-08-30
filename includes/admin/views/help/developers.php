08fa4469253140f220735f97a2ed7d858ba8c6ec
Tim Hughes <timbocode@users.noreply.github.com>
2020-08-30 18:35:01 +0100

    Updates to help pages (#108)
    Minor updates to some help pages to remove some remaining RM blurb.

--- includes/admin/views/help/developers.php
+++ includes/admin/views/help/developers.php
@@ -21,7 +21,7 @@ $code1 = 'add_filter( \'cpseo/metabox/add_seo_metabox\', function( $default ) {
   return $default;
 });';
 ?>
-<p><pre><code><?php esc_html_e($code1); ?></code></pre></p>
+<pre><code><?php esc_html_e($code1); ?></code></pre>
 
 <hr>
 
@@ -32,7 +32,7 @@ $code2 = 'add_filter( \'cpseo/metabox/priority\', function( $priority ) {
   return $priority;
 });';
 ?>
-<p><pre><code><?php esc_html_e($code2); ?></code></pre></p>
+<pre><code><?php esc_html_e($code2); ?></code></pre>
 
 <hr>
 
@@ -40,11 +40,11 @@ $code2 = 'add_filter( \'cpseo/metabox/priority\', function( $priority ) {
 <p><?php esc_html_e( 'This example sets the icon for the custom \'event_cat\' taxonomy to \'dashicons dashicons-category\'.', 'cpseo' ); ?></p>
 <?php
 $code3 = 'add_filter( \'cpseo/taxonomy_icons\', function( $icons ) {
-	$icons[\'event_cat\'] = \'dashicons dashicons-category\';
-	return $icons;
+  $icons[\'event_cat\'] = \'dashicons dashicons-category\';
+  return $icons;
 });';
 ?>
-<p><pre><code><?php esc_html_e($code3); ?></code></pre></p>
+<pre><code><?php esc_html_e($code3); ?></code></pre>
 
 <hr>
 
@@ -52,8 +52,35 @@ $code3 = 'add_filter( \'cpseo/taxonomy_icons\', function( $icons ) {
 <p><?php esc_html_e( 'This example sets the icon for the custom \'event\' post type to \'dashicons dashicons-calendar\'.', 'cpseo' ); ?></p>
 <?php
 $code4 = 'add_filter( \'cpseo/post_type_icons\', function( $icons ) {
-	$icons[\'event\'] = \'dashicons dashicons-calendar\';
-	return $icons;
+  $icons[\'event\'] = \'dashicons dashicons-calendar\';
+  return $icons;
 });';
 ?>
-<p><pre><code><?php esc_html_e($code4); ?></code></pre></p>
+<pre><code><?php esc_html_e($code4); ?></code></pre>
+
+<hr>
+
+<h4><?php esc_html_e( 'Remove all Classic SEO data from the database on uninstall', 'cpseo' ); ?></h4>
+<p><?php esc_html_e( 'Filter to remove Classic SEO data from the database', 'cpseo' ); ?></p>
+
+<?php
+$code5 = 'add_filter( \'cpseo_clear_data_on_uninstall\', \'__return_true\' )';
+?>
+<pre><code><?php esc_html_e($code5); ?></code></pre>
+
+<hr>
+
+<h4><?php esc_html_e( 'Add Custom Power Words', 'cpseo' ); ?></h4>
+<p><?php esc_html_e( 'Filter to add custom Power Words', 'cpseo' ); ?></p>
+
+<?php
+$code6 = 'add_filter( \'cpseo/metabox/power_words\', function( $words ) {
+  $new_words = [
+    \'power-word1\',
+    \'power-word2\',
+    \'power-word3\',
+  ];
+  return array_merge( $words, $new_words );
+} );';
+?>
+<pre><code><?php esc_html_e($code6); ?></code></pre>
--- includes/modules/woocommerce/views/help.php
+++ includes/modules/woocommerce/views/help.php
@@ -9,21 +9,19 @@
 use Classic_SEO\Helper;
 ?>
 
-<h3><?php esc_html_e( 'WooCommerce', 'cpseo' ); ?></h3>
+<h3><?php esc_html_e( 'WooCommerce and Classic Commerce', 'cpseo' ); ?></h3>
 
-<p><?php esc_html_e( 'SEO is the backbone of any website and it couldn\'t be more true for a WooCommerce store.', 'cpseo' ); ?></p>
+<p><?php esc_html_e( 'SEO is an important part of any website and that\'s especially true for a WooCommerce or Classic Commerce store.', 'cpseo' ); ?></p>
 
-<p><?php esc_html_e( 'When you sell something online, you want people to buy it. And, SEO is the best way to do so in the long run.', 'cpseo' ); ?></p>
+<p><?php esc_html_e( 'With the Classic SEO plugin, you can easily optimize your WooCommerce or Classic Commerce store in general and product pages in particular.', 'cpseo' ); ?></p>
 
-<p><?php esc_html_e( 'With the Classic SEO plugin, you can easily optimize your WooCommerce store in general and product pages in particular.', 'cpseo' ); ?></p>
-
-<p><strong><?php esc_html_e( 'Optimizing Your WooCommerce Store', 'cpseo' ); ?></strong></p>
+<p><strong><?php esc_html_e( 'Optimizing Your WooCommerce or Classic Commerce Store', 'cpseo' ); ?></strong></p>
 
 <p>
 	<?php
 	printf(
 		/* translators: link to local seo settings */
-		__( 'Classic SEO can help you make your product category or tag archives `noindex`. You can do that from <a href="%1$s">WordPress Dashboard > Classic SEO > Titles & Meta > Product Categories</a>', 'cpseo' ),
+		__( 'Classic SEO can help you make your product category or tag archives `noindex`. You can do that from <a href="%1$s">ClassicPress Dashboard > Classic SEO > Titles & Meta > Product Categories</a>', 'cpseo' ),
 		Helper::get_admin_url( 'options-titles#setting-panel-taxonomy-product_cat' )
 	);
 	?>
@@ -32,10 +30,8 @@ use Classic_SEO\Helper;
 <p><?php esc_html_e( 'or', 'cpseo' ); ?></p>
 
 <p>
-	<a href="<?php echo Helper::get_admin_url( 'options-titles#setting-panel-taxonomy-product_tag' ); ?>"><?php esc_html_e( 'WordPress Dashboard > Classic SEO > Titles & Meta > Product Tags.', 'cpseo' ); ?></a>
+	<a href="<?php echo Helper::get_admin_url( 'options-titles#setting-panel-taxonomy-product_tag' ); ?>"><?php esc_html_e( 'ClassicPress Dashboard > Classic SEO > Titles & Meta > Product Tags.', 'cpseo' ); ?></a>
 </p>
-
-<p><img src="<?php echo cpseo()->plugin_url() . 'assets/admin/img/help/product-archive-settings.jpg'; ?>" alt="make categories noindex" /></p>
 
 <p><strong><?php esc_html_e( 'Optimizing Your Product Pages', 'cpseo' ); ?></strong></p>
 
@@ -43,42 +39,26 @@ use Classic_SEO\Helper;
 	<?php
 	printf(
 		/* translators: link to local seo settings */
-		__( 'You can customize and automate the SEO Title/Description generation easily as well. Just head over to <a href="%1$s">WordPress Dashboard > Classic SEO > Titles & Meta > Products</a>', 'cpseo' ),
+		__( 'You can customize and automate the SEO Title/Description generation easily as well. Go to <a href="%1$s">ClassicPress Dashboard > Classic SEO > Titles & Meta > Products</a>', 'cpseo' ),
 		Helper::get_admin_url( 'options-titles#setting-panel-post-type-product' )
 	);
 	?>
 </p>
 
-<p><img src="<?php echo cpseo()->plugin_url() . 'assets/admin/img/help/individual-product-settings.jpg'; ?>" alt="product seo title" /></p>
+<p><?php esc_html_e( 'Classic SEO also lets you easily add rich snippets to your product.', 'cpseo' ); ?></p>
 
-<p><?php esc_html_e( 'You can also add rich snippets to your product pages easily with Classic SEO, apart from doing the regular SEO like you would do on posts.', 'cpseo' ); ?></p>
-
-<p>
-	<?php
-	printf(
-		/* translators: link to local seo settings */
-		__( 'Do that from the product pages themeselve. Go to <a href="%1$s">WordPress Dashboard > Products > Add New</a>', 'cpseo' ),
-		admin_url( 'post-new.php?post_type=product' )
-	);
-	?>
-</p>
-
-<p><?php esc_html_e( 'And, choose the product schema from the Rich Snippets tab.', 'cpseo' ); ?></p>
-
-<p><img src="<?php echo cpseo()->plugin_url() . 'assets/admin/img/help/product-rich-snippets.jpg'; ?>" alt="product rich snippets" /></p>
+<p><?php esc_html_e( 'Rich snippets can be added from within the product pages. Simply click on the Rich Snippet tab and choose the Product snippet type option.', 'cpseo' ); ?></p>
 
 <p><strong><?php esc_html_e( 'Optimizing Your Product URLs', 'cpseo' ); ?></strong></p>
 
-<p><?php esc_html_e( 'Classic SEO offers you to remove category base from your product archive URLs so the URLs are cleaner, more SEO friendly and easier to remember.', 'cpseo' ); ?></p>
+<p><?php esc_html_e( 'Classic SEO offers you the option to remove the category base from your product archive URLs so the URLs are cleaner, more SEO friendly and easier to remember.', 'cpseo' ); ?></p>
 
 <p>
 	<?php
 	printf(
 		/* translators: link to local seo settings */
-		__( 'To access those options, head over to <a href="%1$s">WordPress Dashboard > Classic SEO > General Settings > WooCommerce</a>.', 'cpseo' ),
+		__( 'To access those options, head over to <a href="%1$s">ClassicPress Dashboard > Classic SEO > General Settings > WooCommerce</a>.', 'cpseo' ),
 		Helper::get_admin_url( 'options-general#setting-panel-woocommerce' )
 	);
 	?>
 </p>
-
-<p><img src="<?php echo cpseo()->plugin_url() . 'assets/admin/img/help/woocommerce-url-settings.jpg'; ?>" alt="product category base" /></p>
